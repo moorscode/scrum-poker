d2f631a93d82458222792f61e79448200435af0e
Jip <jhp.moors@gmail.com>
2020-11-28 15:19:57 +0100

    Add story history

--- src/points/points.service.ts
+++ src/points/points.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
+interface points {
+  [index: number]: number | string;
+}
+
 @Injectable()
 export class PointsService {
-  public static getPoints() {
+  public static getPoints(): any[] {
     return [0, 0.5, 1, 2, 3, 5, 8, 13, 21, 100, 'coffee'];
   }
 }
