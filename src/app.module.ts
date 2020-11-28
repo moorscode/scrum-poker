e71279306b474afa5bffcd75c25419b197a0627c
Jip <jhp.moors@gmail.com>
2020-11-28 15:32:35 +0100

    Provide all services
    Just for neatness.
    Not needed at the moment.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PokersModule } from './pokers/pokers.module';
 import { PointsService } from './points/points.service';
+import { PokersRoomsService } from './pokers/pokers-rooms.service';
 
 @Module({
   imports: [PokersModule],
   controllers: [],
-  providers: [PointsService],
+  providers: [PointsService, PokersRoomsService],
 })
 export class AppModule {}
